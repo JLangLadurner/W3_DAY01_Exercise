6cdb59c28c6ded586869ad113bd97564dc8887fc
Simon Blaha <s.blaha@live.at>
2019-05-20 09:25:48 +0200

    push basic exercise A0

--- src/Basic_A0/Basic_A0.java
+++ src/Basic_A0/Basic_A0.java
@@ -1,8 +1,32 @@
 package Basic_A0;
 
+import java.util.ArrayList;
+
 public class Basic_A0 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        ArrayList<String> cities = new ArrayList<String>();
+
+        cities.add("Vienna");
+        cities.add("Graz");
+        cities.add("Linz");
+        cities.add("Wels");
+        cities.add("Klagenfurt");
+        cities.add("Sankt Pölten");
+
+        System.out.println(cities);
+
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        
+        temp.add(10);
+        temp.add(22);
+        temp.add(15);
+        temp.add(30);
+        temp.add(32);
+        temp.add(18);
+
+        System.out.println(temp);
+        }
     }
-}
+
