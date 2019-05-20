6ab98ae8d087dd4d42a51aed5aa4d65608a354cc
judith <judithmarialang@gmail.com>
2019-05-20 12:00:18 +0200

    adds Intermediate A2 exercise

--- src/Intermediate_A2/Intermediate_A2.java
+++ src/Intermediate_A2/Intermediate_A2.java
@@ -1,4 +1,44 @@
 package Intermediate_A2;
 
+import java.util.ArrayList; //import for ArrayList!!
+import java.util.Collections;
+
 public class Intermediate_A2 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> animals = new ArrayList<String>();
+
+        animals.add("dog");
+        animals.add("bird");
+        animals.add("cat");
+        animals.add("cow");
+
+        for (String i : animals) {
+            System.out.println(animals);
+        }
+
+        animals.remove(2); //removes
+
+        for (String i : animals){
+            System.out.println(animals);
+        }
+        animals.add(2,"cats");
+        for (String i : animals){
+            System.out.println(animals);
+        }
+         animals.set(1, "birdie");
+
+        for (String i : animals){
+            System.out.println(animals);
+        }
+
+        Collections.reverse(animals);
+        for (String i : animals){
+            System.out.println(animals);
+        }
+
+
+
+    }
 }
